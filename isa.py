82d85444ecb810e1cc81efa590f0b20c115965d0
Dale McDiarmid <dalem@elastic.co>
2018-11-03 11:08:10 +0000

    Basic implementation

--- isa.py
+++ isa.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def isa (fobj, bounds, dimensions, alpha=0.2, popsize=10, its=1000):
+def isa (fobj, bounds, dimensions, alpha=0.2, popsize=20, its=1000):
     #init the population
     init_pop = np.random.rand(popsize, dimensions)
     Lb, Ub = np.asarray(bounds).T
@@ -9,13 +9,13 @@ def isa (fobj, bounds, dimensions, alpha=0.2, popsize=10, its=1000):
     diff = np.fabs(Lb - Ub)
     pop = Lb + init_pop * diff
     #find fitnesses
-    fitnesses = np.asarray([fobj(ind) for ind in pop])
+    fit = np.asarray([fobj(ind) for ind in pop])
     #find the best
-    x_gb_idx = np.argmin(fitnesses)
-    print(x_gb_idx)
+    x_gb_idx = np.argmin(fit)
     gb=pop[x_gb_idx]
     for j in range(its):
         pop_new = np.zeros((popsize, dimensions))
+        fit_new = np.zeros(popsize)
         # find the minimal and maxima of each dimension
         Lb_j = pop.min(axis=0)
         Ub_j = pop.max(axis=0)
@@ -32,8 +32,18 @@ def isa (fobj, bounds, dimensions, alpha=0.2, popsize=10, its=1000):
                 else:
                     # compositon group
                     r_2 = np.random.rand(1, 1)[0][0]
-                    x_i_j = Lb_j + (Ub_j-Lb_j)*r_2
-                    pop_new[i] = x_i_j
-                    pass
+                    pop_new[i] = Lb_j + (Ub_j-Lb_j)*r_2
+            # calculate new fitness
+            fit_new[i] = fobj(pop_new[i])
+            # apply the evolutionary bounding constraint scheme to the new point
 
-isa(lambda x: sum(x**2)/len(x),[(-5, 5)],10)
+
+        for i in range(popsize):
+            # determine points to keep
+            # we could do this inline above?
+            if fit_new[i] < fit[i]:
+                fit[i] = fit_new[i]
+                pop[i] = pop_new[i]
+    return fit, pop
+
+print(isa(lambda x: sum(x**2),[(-5, 5)],10))
