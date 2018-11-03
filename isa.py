58821725a3dd42b51b1773957cb59f7ae50ee1d1
Dale McDiarmid <dalem@elastic.co>
2018-11-03 12:11:04 +0000

    With evolutionary bounding constraint

--- isa.py
+++ isa.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-
-def isa (fobj, bounds, dimensions, alpha=0.2, popsize=20, its=1000):
+def isa (fobj, bounds, alpha=0.2, popsize=20, its=10000):
     #init the population
+    dimensions=len(bounds)
     init_pop = np.random.rand(popsize, dimensions)
     Lb, Ub = np.asarray(bounds).T
     gb_lam = (Ub - Lb) * 0.01;
@@ -10,10 +10,10 @@ def isa (fobj, bounds, dimensions, alpha=0.2, popsize=20, its=1000):
     pop = Lb + init_pop * diff
     #find fitnesses
     fit = np.asarray([fobj(ind) for ind in pop])
-    #find the best
-    x_gb_idx = np.argmin(fit)
-    gb=pop[x_gb_idx]
     for j in range(its):
+        # find the best
+        x_gb_idx = np.argmin(fit)
+        gb = pop[x_gb_idx]
         pop_new = np.zeros((popsize, dimensions))
         fit_new = np.zeros(popsize)
         # find the minimal and maxima of each dimension
@@ -21,23 +21,33 @@ def isa (fobj, bounds, dimensions, alpha=0.2, popsize=20, its=1000):
         Ub_j = pop.max(axis=0)
         for i in range(popsize):
             if j == x_gb_idx:
-                #global best - do some random walk
+                # global best - do some random walk
                 pop_new[i] = pop[i]+(np.random.normal(size=dimensions) * gb_lam)
             else:
                 if np.random.rand(1, 1) < alpha:
-                    #mirror group
+                    # mirror group
                     r_3 = np.random.rand(1, 1)[0][0]
                     xm_i_j = pop[i]*r_3 + ((1 -r_3)*gb)
                     pop_new[i] = (2 * xm_i_j) - pop[i]
                 else:
-                    # compositon group
+                    # composition group
                     r_2 = np.random.rand(1, 1)[0][0]
                     pop_new[i] = Lb_j + (Ub_j-Lb_j)*r_2
+            # apply the evolutionary bounding constraint scheme to the new point
+            is_lower = np.argwhere(pop_new[i] < Lb)
+            is_greater = np.argwhere(pop_new[i] > Ub)
+            if is_lower.any():
+                # apply the lower bound constraint
+                # assumption - we use the same random number on all violations
+                a = np.random.rand(1, 1)
+                pop_new[i][is_lower]= (a * Lb[is_lower]) + ((1-a)*gb[is_lower])
+            if is_greater.any():
+                # apply the upper bound constraint
+                # assumption - we use the same random number on all violations
+                b = np.random.rand(1, 1)
+                pop_new[i][is_greater] = (b * Ub[is_greater]) + (1 - b) * gb[is_greater]
             # calculate new fitness
             fit_new[i] = fobj(pop_new[i])
-            # apply the evolutionary bounding constraint scheme to the new point
-
-
         for i in range(popsize):
             # determine points to keep
             # we could do this inline above?
@@ -46,4 +56,5 @@ def isa (fobj, bounds, dimensions, alpha=0.2, popsize=20, its=1000):
                 pop[i] = pop_new[i]
     return fit, pop
 
-print(isa(lambda x: sum(x**2),[(-5, 5)],10))
+#test functions sum(x^2)
+print(isa(lambda x: sum(x**2),[(-5, 5)]*10,alpha=0.2))
