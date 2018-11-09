f61e0a1fc9310758a4106cb542354ba584e541ca
Dale McDiarmid <dalem@elastic.co>
2018-11-09 15:20:32 +0000

    isa tests

--- isa_tests.py
+++ isa_tests.py
@@ -82,5 +82,3 @@ isa_runner(lambda x: rastigins(x), 'Rastigins', [(-5.12, 5.12)]*10, its=30, pops
 isa_runner(lambda x: rastigins(x), 'Rastigins', [(-5.12, 5.12)]*10, its=30, f_alpha=lambda i: random.uniform(0.1, 0.2), popsize=50, evals=10000)
 # increasing alpha
 isa_runner(lambda x: rastigins(x), 'Rastigins', [(-5.12, 5.12)]*10, its=30, f_alpha=lambda i: 0.1+ (i * (0.2/1000)), popsize=50, evals=10000)
-
-
