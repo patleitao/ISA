1f2f3bd82ac181e2f3f6634a3aa46f939fbe48f8
Dale McDiarmid <dalem@elastic.co>
2018-11-09 15:43:45 +0000

    Fix order

--- isa_tests.py
+++ isa_tests.py
@@ -9,22 +9,9 @@ from isa import isa_runner
 from pso import pso_runner
 
 
-#ISA Test Runs
-# Constant alpha - 0.2, 5 iterations, 50 pop size, 1000 evals per iteration
-#isa_runner(lambda x: ((10**5)*x[0]**2) + x[1]**2 - (x[0]**2+x[1]**2)**2 + 10**-5*(x[0]**2 + x[1]**2)**4, 'Dekkers and Aarts',
-#         [(-20, 20)]*2, its=5, alpha_ranges=[0.2], popsize=50, evals=1000)
-
-# Linearly increasing alpha - 0.1 to 0.3, 5 iterations, 50 pop size, 1000 evals per iteration
-#isa_runner(lambda x: ((10**5)*x[0]**2) + x[1]**2 - (x[0]**2+x[1]**2)**2 + 10**-5*(x[0]**2 + x[1]**2)**4, 'Dekkers and Aarts',
-#          [(-20, 20)]*2, its=5, alpha_ranges=[0.1+ (i * (0.2/1000)) for i in range(1000)], popsize=50, evals=1000)
-
-# Constant w - 0.9, 5 iterations, 50 pop size, 1000 evals per iteration
-#pso_runner(lambda x: ((10**5)*x[0]**2) + x[1]**2 - (x[0]**2+x[1]**2)**2 + 10**-5*(x[0]**2 + x[1]**2)**4, 'Dekkers and Aarts',
-#           [(-20, 20)]*2, its=2, w_ranges=[0.9], popsize=50, evals=1000)
-
-# Linearly decreasing w - 1.1 to 0.1 (as in paper), 5 iterations, 50 pop size, 1000 evals per iteration
-#pso_runner(lambda x: ((10**5)*x[0]**2) + x[1]**2 - (x[0]**2+x[1]**2)**2 + 10**-5*(x[0]**2 + x[1]**2)**4, 'Dekkers and Aarts',
-#           [(-20, 20)]*2, its=2, w_ranges=[1.1 - (i * (1.0/1000)) for i in range(1000)], popsize=50, evals=1000)
+# ISA Easom Test Runs
+def easom(x,y):
+    return -np.cos(x) * np.cos(y) * np.exp( -np.power((x-np.pi),2) - np.power(y-np.pi,2) )
 
 
 # constant alpha
@@ -62,10 +49,6 @@ isa_runner(lambda x: kowalik(x[0],x[1],x[2],x[3]), 'Kowalik', [(-5, 5)]*4, its=3
 isa_runner(lambda x: kowalik(x[0],x[1],x[2],x[3]), 'Kowalik', [(-5, 5)]*4, its=30, f_alpha=lambda i: random.uniform(0.1, 0.2), popsize=50, evals=1000)
 # increasing alpha
 isa_runner(lambda x: kowalik(x[0],x[1],x[2],x[3]), 'Kowalik', [(-5, 5)]*4, its=30, f_alpha=lambda i: 0.1+ (i * (0.2/1000)), popsize=50, evals=1000)
-
-# ISA Easom Test Runs
-def easom(x,y):
-    return -np.cos(x) * np.cos(y) * np.exp( -np.power((x-np.pi),2) - np.power(y-np.pi,2) )
 
 
 def rastigins(x):
