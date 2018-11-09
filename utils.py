1e327144ff4cec7d680d34cf7e3fd639ede58981
Dale McDiarmid <dalem@elastic.co>
2018-11-09 18:03:48 +0000

    Record every 10

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 import math
 
 
+#def recording_intervals(evals):
+#    max = int(math.ceil(math.log(evals) / math.log(2)))
+#    max = max if max <= 10 else 10
+#    intervals = [2 ** i for i in range(1,max)]
+#    remain = int(math.ceil((evals - (2**max))/1000))
+#    intervals.extend([i*1000 for i in range(1,remain+1)])
+#    intervals.append(evals)
+#    return intervals
+
+
 def recording_intervals(evals):
-    max = int(math.ceil(math.log(evals) / math.log(2)))
-    max = max if max <= 10 else 10
-    intervals = [2 ** i for i in range(1,max)]
-    remain = int(math.ceil((evals - (2**max))/1000))
-    intervals.extend([i*1000 for i in range(1,remain+1)])
-    intervals.append(evals)
-    return intervals
+    return [i*10 for i in range(1,math.ceil(evals/10)+1)]
