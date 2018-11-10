9065b6f9bbd49c6ef11a77ba004499f08979f40c
Dale McDiarmid <dalem@elastic.co>
2018-11-10 23:24:22 +0000

    Cleanup

--- isa_sphere.py
+++
@@ -1,6 +0,0 @@
-import random
-from isa import isa_runner
-from benchmark_functions import *
-
-# increasing alpha
-isa_runner(lambda x: sphere(x), 'Sphere', [(-100, 100)]*20, its=30, f_alpha=lambda i: 0.1+ (i * (0.2/10000)), popsize=50, evals=10000)
--- pso_ackley.py
+++
@@ -1,7 +0,0 @@
-from pso import pso_runner
-from benchmark_functions import *
-
-
-# decreasing w
-pso_runner(lambda x: ackley(x), 'Ackley', [(-32, 32)]*10, its=30, w_ranges=[1.1 - (w * (1.0/10000)) for w in range(10000)], popsize=50, evals=10000)
-
